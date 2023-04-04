7f3ec475ccbbbf7e8ffa2787a1090d7baf536854
stefanossandrade <stefano.ss.andrade@hotmail.com>
2023-04-04 15:52:09 -0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Posts } from "./components/Posts"
 import "./global.css"
+import styles from "./App.module.css"
 
 export const App = () => {
-  
+
   return (
-    <>
+    <div className={styles.mainContainer}>
       <Posts />
-    </>
+    </div>
   )
 }
 
